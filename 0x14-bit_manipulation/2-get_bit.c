2017c7cc8d006375038b1956fff87c15b1617cd2
codeegirl0 <asmaelk65@gmail.com>
2023-08-31 10:56:11 -0700

    adding solution file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
-	value_bit= (n >> index) & 1;
+	value_bit = (n >> index) & 1;
 
 	return (value_bit);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - to count a number of bits to change to get from one number to another
+ * flip_bits - to count a number of bits to 
+ * change to get from one number to another
  * @n: the first number
  * @m: the second number
  * Return: number of bits to be changed
