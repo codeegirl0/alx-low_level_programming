dde58b73809ad044776691a58e475e93d66d62c4
codeegirl0 <asmaelk65@gmail.com>
2023-10-20 13:33:25 -0700

    changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/codeegirl0/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/codeegirl0/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
